d434d69cccd2229cd1cbabacfc6515e32871e70c
Eshika Gupta <eshikagupta001@gmail.com>
2024-12-25 22:47:39 +0530

    Updated code for disabling quiz buttons when one is selected

--- app/course/[courseId]/quiz/_components/QuizCardItem.jsx
+++ app/course/[courseId]/quiz/_components/QuizCardItem.jsx
@@ -1,9 +1,18 @@
 import { Button } from '@/components/ui/button'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function QuizCardItem({quiz,userSelectedOption, userHasAnswered }) {
-    const [selectedOption,setSelctedOption]=useState()
-    
+    const [selectedOption,setSelectedOption]=useState(null)
+    const handleOptionClick = (option) => {
+      if (userHasAnswered || selectedOption) return; // Prevent further clicks
+      setSelectedOption(option);
+      userSelectedOption(option);
+  };
+
+  useEffect(() => {
+    setSelectedOption(null);
+}, [quiz]);
+
   return quiz&&(
     <div className='mt-8 p-5'>
         <h2 className='font-medium mb-7 text-2xl text-center'>
@@ -12,9 +21,15 @@ function QuizCardItem({quiz,userSelectedOption, userHasAnswered }) {
         <div className='grid grid-cols-2 gap-5'>
             {quiz?.options.map((option,index)=>(
                 <h2 
-                onClick={()=>{setSelctedOption(option);userSelectedOption(option)}} disabled={userHasAnswered} key={index} variant='outline' 
-                className={`hover:bg-gray-200 cursor-pointer border rounded-full px-2 mt-1 text-lg 'bg-black w-full py-3 text-center 
-                ${option==selectedOption&&'bg-slate-200 border-black hover:text-black'}`}>{option}</h2>
+                onClick={() => handleOptionClick(option)} key={index} variant='outline' 
+                className={`hover:bg-gray-200 cursor-pointer border rounded-full px-2 mt-1 text-lg w-full py-3 text-center 
+                  ${
+                      option === selectedOption
+                          ? 'bg-slate-200 border-black hover:text-black'
+                          : ''
+                  }
+                  ${selectedOption ? 'cursor-not-allowed opacity-50' : ''}`}
+              style={{ pointerEvents: selectedOption ? 'none' : 'auto' }}>{option}</h2>
             ))}
             
         </div>
